5d663621f7f07ac7bb605de5e52c12313d67c4f3
SynysterRudro <rudrosingh82@gmail.com>
2022-10-27 20:49:28 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 const courses = require('./data/courses.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello world');
